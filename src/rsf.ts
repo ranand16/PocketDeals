ae9d0074befe4f71ae5b3e1520a74c66e933744a
RIshabh Anand <ranand16@gmail.com>
2021-04-14 07:56:34 +0530

    - Adding firebase main app as well

--- src/rsf.ts
+++ src/rsf.ts
@@ -2,7 +2,7 @@ import firebase from 'firebase'
 import '@firebase/firestore'
 import ReduxSagaFirebase from 'redux-saga-firebase'
 
-const firebaseApp = firebase.initializeApp({
+export const firebaseApp = firebase.initializeApp({
   apiKey: "AIzaSyDqetdyZP3OSZNhRT0fxdXnB6MO8Cy2ZI4",
   authDomain: "cloud-checkup.firebaseapp.com",
   databaseURL: "https://cloud-checkup.firebaseio.com",
@@ -12,7 +12,6 @@ const firebaseApp = firebase.initializeApp({
   appId: "1:616498626633:web:3aa565d865b57c09767f98",
   measurementId: "G-D1CB2MECFM"
 })
-
 const rsf = new ReduxSagaFirebase(firebaseApp)
 
 export default rsf
